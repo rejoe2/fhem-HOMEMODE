4bfc67c90cfcc088c802de518809bd4fba8504b8
DeeSPe <ds@deespe.com>
2018-01-16 23:33:37 +0100

    v1.4.2 check-in 2

--- FHEM/22_HOMEMODE.pm
+++ FHEM/22_HOMEMODE.pm
@@ -1,5 +1,5 @@
 #####################################################################################
-# $Id: 22_HOMEMODE.pm 15908 2018-01-16 21:40:30Z DeeSPe $
+# $Id: 22_HOMEMODE.pm 15909 2018-01-16 22:32:51Z DeeSPe $
 #
 # Usage
 #
@@ -3762,8 +3762,8 @@ sub HOMEMODE_Details($$$)
     </li>
     <li>
       <b><i>HomeAutoPresenceSuppressState</i></b><br>
-      suppress mode(s) for HomeAutoPresence (p.e. gotosleep|asleep)<br>
-      if set this/these mode(s) of a resident will not affect the residents to change to absent by its presence device<br>
+      suppress state(s) for HomeAutoPresence (p.e. gotosleep|asleep)<br>
+      if set this/these state(s) of a resident will not affect the residents to change to absent by its presence device<br>
       p.e. for misteriously disappearing presence devices in the middle of the night<br>
       default:
     </li>
